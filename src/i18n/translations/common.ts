c3290e0017f4b815e8f3a081934647f3e5ea50ff
Ivan Fraixedes <ivan@fraixed.es>
2026-06-12 17:12:13 +0200

    Add the og image and related meta tags
    Add the open graph image and add the tags associated to it and include
    the alternative text in the 3 languages.

--- src/i18n/translations/common.ts
+++ src/i18n/translations/common.ts
@@ -12,6 +12,8 @@ export const common = {
     'theme.toggle.aria': 'Canviar tema clar/fosc',
     'theme.light': 'LIGHT_MODE',
     'theme.dark': 'DARK_MODE',
+
+    'ogImage.alt': 'Ciutat de Girona al fons (edificis, la Catedral, etc.) i al davant el logo de GeeksCAT (cervell amb línies de circuit) tot això sobre un terra que represente un circuit imprès',
   },
   en: {
     'tba.status': 'COMING SOON',
@@ -26,6 +28,8 @@ export const common = {
     'theme.toggle.aria': 'Toggle light/dark theme',
     'theme.light': 'LIGHT_MODE',
     'theme.dark': 'DARK_MODE',
+
+    'ogImage.alt': 'At the back, the Girona City (buldings, the Cathedral, etc.) and hovered by the GeeksCAT logo (brain with the integrated circuit lines), everything on a floor which represents a integrated circuit',
   },
   es: {
     'tba.status': 'PRÓXIMAMENTE',
@@ -40,5 +44,7 @@ export const common = {
     'theme.toggle.aria': 'Cambiar tema claro/oscuro',
     'theme.light': 'LIGHT_MODE',
     'theme.dark': 'DARK_MODE',
+
+    'ogImage.alt': 'Ciudad de Girona al fondo (edificios, la Catedral, etc.) y delante el logo de GeeksCAT (celebro con las líneas de circuito) todo esto sobre un suleo que representa un circuito impreso',
   },
 } as const;
